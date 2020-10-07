283a17ac45a267401e98a5a69b2ebc2e579f5856
vladn255 <nesvitvlad@yandex.ru>
2020-10-07 21:15:58 +0400

    Личный проект: больше деталей (часть 1) _ Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const signsList = [];
-const map = document.querySelector(`.map`);
-const mapBlock = map.getBoundingClientRect();
 const MAP_HEIGHT_MAX = 630;
 const MAP_HEIGHT_MIN = 130;
 const TYPE_LIST = [`palace`, `flat`, `house`, `bungalow`];
@@ -10,44 +7,50 @@ const CHECKIN_LIST = [`12:00`, `13:00`, `14:00`];
 const CHECKOUT_LIST = [`12:00`, `13:00`, `14:00`];
 const FEATURES_LIST = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS_LIST = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const AVATAR_NAME_START = `img/avatars/user0`;
+const AVATAR_NAME_END = `.png`;
+const PRICE_MAX = 1000000;
+const ROOMS_COUNT = [1, 2, 3, 100];
+const GUEST_COUNT = [1, 2, 3];
+const PIN_WIDTH = 40;
+const PIN_HEIGHT = 40;
+const AUTHORS_COUNT = 8;
+
+const signsList = [];
+const map = document.querySelector(`.map`);
+const mapBlockWidth = map.offsetWidth;
 const pinTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
 const pinsList = document.querySelector(`.map__pins`);
 
-const randomInteger = (min, max) => {
+const generateRandomInt = (min, max) => {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
-const randomSingleData = (array) => {
-  let min = 0;
-  let max = array.length - 1;
-  return array[randomInteger(min, max)];
+const getRandomSingleData = (array) => {
+  return array[generateRandomInt(0, array.length - 1)];
 };
 
-const randomArrayData = (array) => {
+const getRandomArrayData = (array) => {
   let randomArray = [];
-  let min = 0;
-  let max = array.length - 1;
-  let arrayEntriesNumber = randomInteger(min, max);
+  let arrayEntriesNumber = generateRandomInt(0, array.length - 1);
   for (let i = 0; i <= arrayEntriesNumber; i++) {
-    randomArray.push(randomSingleData(array));
+    randomArray.push(getRandomSingleData(array));
   }
   return randomArray;
 };
 
-const getAvatar = (n) => {
-  let avatarName = `img/avatars/user${`0${n}`}.png`;
+const generateAvatarName = (n) => {
+  let avatarName = AVATAR_NAME_START + n + AVATAR_NAME_END;
   return avatarName;
 };
 
-const getLocation = () => {
-  let mapWidthMin = mapBlock.x;
-  let mapWidthMax = mapBlock.x + mapBlock.width;
+const generateLocation = () => {
   let location = {
-    x: randomInteger(mapWidthMin, mapWidthMax),
-    y: randomInteger(MAP_HEIGHT_MIN, MAP_HEIGHT_MAX)
+    x: generateRandomInt(0, mapBlockWidth),
+    y: generateRandomInt(MAP_HEIGHT_MIN, MAP_HEIGHT_MAX)
   };
   return location;
 };
@@ -59,49 +62,91 @@ const getAddress = (location) => {
   return address;
 };
 
-const createSign = (n) => {
-  let location = getLocation();
+const getPrice = (type) => {
+  let priceMin = 0;
+  switch (type) {
+    case TYPE_LIST[3]:
+      priceMin = 0;
+      break;
+    case TYPE_LIST[2]:
+      priceMin = 5000;
+      break;
+    case TYPE_LIST[1]:
+      priceMin = 1000;
+      break;
+    case TYPE_LIST[0]:
+      priceMin = 10000;
+      break;
+  }
+  let newPrice = generateRandomInt(priceMin, PRICE_MAX);
+  return newPrice;
+};
+
+const getRooms = () => {
+  let roomsCount = ROOMS_COUNT[generateRandomInt(0, ROOMS_COUNT.length - 1)];
+  return roomsCount;
+};
+
+const getGuests = () => {
+  let guestsCount = GUEST_COUNT[generateRandomInt(0, ROOMS_COUNT.length - 1)];
+  return guestsCount;
+};
+
+const generateSign = (n) => {
+  let newLocation = generateLocation();
+  let newType = getRandomSingleData(TYPE_LIST);
   let sign = {
-    avatar: getAvatar(n),
+    avatar: generateAvatarName(n),
     offer: {
       title: `some string`,
-      address: getAddress(location),
-      price: `some integer`,
-      type: randomSingleData(TYPE_LIST),
-      rooms: `some integer`,
-      guests: `some integer`,
-      checkin: randomSingleData(CHECKIN_LIST),
-      checkout: randomSingleData(CHECKOUT_LIST),
-      features: randomArrayData(FEATURES_LIST),
-      description: `some string`,
-      photos: randomArrayData(PHOTOS_LIST)
+      address: getAddress(newLocation),
+      price: getPrice(newType),
+      type: newType,
+      rooms: getRooms(),
+      guests: getGuests(),
+      checkin: getRandomSingleData(CHECKIN_LIST),
+      checkout: getRandomSingleData(CHECKOUT_LIST),
+      features: getRandomArrayData(FEATURES_LIST),
+      description: `some description`,
+      photos: getRandomArrayData(PHOTOS_LIST)
     },
-    location: location
+    location: newLocation
   };
   return sign;
 };
 
 const createPin = (pinInfo) => {
   let newPinItem = pinTemplate.cloneNode(true);
-  newPinItem.style = `left:` + (pinInfo.location.x + 40) + `px; top: ` + (pinInfo.location.y + 40) + `px;`;
+  newPinItem.style.left = `${(pinInfo.location.x - PIN_WIDTH / 2)}px`;
+  newPinItem.style.top = `${(pinInfo.location.y - PIN_HEIGHT)}px`;
+
   let newPinItemImg = newPinItem.querySelector(`img`);
   newPinItemImg.src = pinInfo.avatar;
   newPinItemImg.alt = pinInfo.offer.title;
   return newPinItem;
 };
 
-for (let i = 1; i <= 8; i++) {
-  let newSign = createSign(i);
-  signsList.push(newSign);
-}
+const createPinsFragment = () => {
+  for (let i = 1; i <= AUTHORS_COUNT; i++) {
+    let newSign = generateSign(i);
+    signsList.push(newSign);
+  }
+
+  let pinFragment = document.createDocumentFragment();
+
+  for (let i of signsList) {
+    let newItem = createPin(i);
+    pinFragment.appendChild(newItem);
+  }
+  return pinFragment;
+};
+
+const createPinsList = () => {
+  let pinFragment = createPinsFragment();
+  pinsList.appendChild(pinFragment);
+};
 
 map.classList.remove(`map--faded`);
 
-const pinFragment = document.createDocumentFragment();
+createPinsList();
 
-for (let i of signsList) {
-  let newItem = createPin(i);
-  pinFragment.appendChild(newItem);
-}
-
-pinsList.appendChild(pinFragment);
